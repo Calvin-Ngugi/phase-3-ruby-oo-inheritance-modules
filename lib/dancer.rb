3c979d2874d6a733cc3c8563d3b4b439668460f1
calvin <ncalvin67@gmail.com>
2022-10-24 22:43:58 +0300

    completed assignment

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,7 +1,3 @@
-require_relative './dance_module'
-require_relative './meta_dancing_module'
-require_relative './fancy_dance.rb'
-
 class Dancer
     include Dance
     extend MetaDancing
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,7 +1,3 @@
-require_relative './dance_module'
-require_relative './meta_dancing_module'
-require_relative './fancy_dance.rb'
-
 class Kid
     include Dance
     extend MetaDancing
